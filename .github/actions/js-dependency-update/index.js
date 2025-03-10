3b12815c0d27f8c4295f0f0e129e88943ff236b1
yashUcr773 <yagga004@ucr.edu>
2025-03-10 21:19:17 +0530

    js action pr bot

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,88 @@
-const core = require('@actions/core');
- 
-async function run() { 
-  core.info('I am a custom JS action');
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const github = require("@actions/github");
+
+const validateBranchName = ({ branchName }) => {
+  const regex = /^[a-zA-Z0-9_\.\/]+$/;
+  return regex.test(branchName);
+};
+
+async function run() {
+  const baseBranch = core.getInput("base-branch", { required: true });
+  const targetBranch = core.getInput("target-branch", { required: true });
+  const workingDirectory = core.getInput("working-directory", {
+    required: true,
+  });
+  const ghToken = core.getInput("gh-token", { required: true });
+
+  core.setSecret(ghToken);
+
+  if (!validateBranchName({ branchName: baseBranch })) {
+    return core.setFailed("Invalid base branch name.");
+  }
+
+  if (!validateBranchName({ branchName: targetBranch })) {
+    return core.setFailed("Invalid target branch name.");
+  }
+
+  if (!validateBranchName({ branchName: workingDirectory })) {
+    return core.setFailed("Invalid working directory name.");
+  }
+
+  core.info(`[js-dependency-update]: base-branch: ${baseBranch}`);
+  core.info(`[js-dependency-update]: base-branch: ${targetBranch}`);
+  core.info(`[js-dependency-update]: working-directory: ${workingDirectory}`);
+
+  await exec.exec("npm update", [], {
+    cwd: workingDirectory,
+  });
+
+  const gitStatus = await exec.getExecOutput(
+    "git status -s package*.json",
+    [],
+    {
+      cwd: workingDirectory,
+    }
+  );
+
+  if (gitStatus.stdout.length > 0) {
+    core.info("[js-dependency-update]: There are some updates available!");
+    await exec.exec('git config --global user.name "PR_BOT"');
+    await exec.exec('git config --global user.email "PR_BOT@email.com"');
+    await exec.exec(`git checkout -b ${targetBranch}`, [], {
+      cwd: workingDirectory,
+    });
+    await exec.exec(`git add package.json package-lock.json`, [], {
+      cwd: workingDirectory,
+    });
+    await exec.exec(`git commit -m "update dependencies"`, [], {
+      cwd: workingDirectory,
+    });
+    await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+      cwd: workingDirectory,
+    });
+
+    const octokit = github.getOctokit(ghToken);
+
+    try {
+      await octokit.rest.pulls.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        title: "Update NPM dependencies",
+        body: "Update Body",
+        base: baseBranch,
+        head: targetBranch,
+      });
+    } catch (e) {
+      core.error(
+        `[js-dependency-update]: Something went wrong while creating PR.`
+      );
+      core.setFailed(e.message);
+      core.error(e);
+    }
+  } else {
+    core.info("[js-dependency-update]: There are no updates available!");
+  }
 }
- 
+
 run();
