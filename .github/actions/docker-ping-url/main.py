436f3fd0dc5d58f27357287554f1ee714f37e628
yashUcr773 <yagga004@ucr.edu>
2025-03-10 22:45:02 +0530

    added logic

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,39 @@
+import requests
+import os
+import time
+
+def ping_url(url, delay, max_trials):
+    trials = 0
+    while trials < max_trials:
+        print (f"Trial {trials+1} of {max_trials}")
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable.")
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is unreachable. Retrying in {delay} seconds")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid url ${url}")
+            return False
+        
+    return False
+
+
+
+def run():
+    website_url=os.getenv("INPUT_URL")
+    delay=os.getenv("INPUT_DELAY")
+    max_trails=os.getenv("INPUT_MAX_TRIALS")
+
+    website_reachable = ping_url(website_url, delay, max_trails)
+
+    if not website_reachable:
+        raise Exception(f"Website URL {website_url} is not reachable.")
+    
+    print (f"Website URL {website_url} is reachable.")
+
 if __name__ == "__main__":
-    print('Hello World')
+    run()
